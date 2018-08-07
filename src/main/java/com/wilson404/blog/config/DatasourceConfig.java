26371cb1767ed0310939d36c74f038bd80aab69d
wilson wang <wilson404@wilson404.com>
2018-08-07 12:55:07 +0800

    1.add c3p0 datasource

---
+++ src/main/java/com/wilson404/blog/config/DatasourceConfig.java
@@ -0,0 +1,32 @@
+package com.wilson404.blog.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatasourceConfig {
+
+    private final Environment env;
+
+    @Autowired
+    public DatasourceConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        return DataSourceBuilder.create()
+                .type(ComboPooledDataSource.class)
+                .driverClassName(env.getProperty("spring.datasource.driver-class-name"))
+                .url(env.getProperty("spring.datasource.url"))
+                .username(env.getProperty("spring.datasource.username"))
+                .password(env.getProperty("spring.datasource.password"))
+                .build();
+    }
+}
